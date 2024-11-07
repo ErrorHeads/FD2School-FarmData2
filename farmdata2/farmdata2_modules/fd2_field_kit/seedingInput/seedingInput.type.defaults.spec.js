2d3f2ea111e15ebe507f1a5ccf7aeea195716c16
FoxFGC <frbecker19@gmail.com>
2024-11-07 11:40:44 -0500

    Laid out the testing file for the seeding input defaults, actual tests not yet implemented

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+describe('Test the default contents of the "Seeding Type" section of the Seeding Input Form in the FieldKit.', () => {
+
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-field-kit/seedingInput')
+    })
+
+    it('Check that there is an element for Tray and Direct and that they are enabled', () => {
+
+    })
+
+    it('Check that neither the Tray or Direct elements are selected, and that the message indicating one but be selected is visible', () => {
+
+    })
+
+    it('Check that neither form elements for the Tray and Direct seedings are visible', () => {
+
+    })
+
+
+})
